defbc4eae2485f24b865c23f082f638b154a5eb8
Tom Benyon <Tom.Benyon@creative-assembly.com>
2019-12-21 20:51:28 +0000

    Tweak deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,26 +3,19 @@
 #
 # Steps to deploying:
 #
-#  1. Ask for plugin slug.
-#  2. Ask for local plugin directory.
-#  3. Check local plugin directory exists.
-#  4. Ask for main plugin file name.
-#  5. Check main plugin file exists.
-#  6. Check readme.txt version matches main plugin file version.
-#  7. Ask for temporary SVN path.
-#  8. Ask for remote SVN repo.
-#  9. Ask for SVN username.
-# 10. Ask if input is correct, and give chance to abort.
-# 11. Check if Git tag exists for version number (must match exactly).
-# 12. Checkout SVN repo.
-# 13. Set to SVN ignore some GitHub-related files.
-# 14. Export HEAD of master from git to the trunk of SVN.
-# 15. Initialise and update and git submodules.
-# 16. Move /trunk/assets up to /assets.
-# 17. Move into /trunk, and SVN commit.
-# 18. Move into /assets, and SVN commit.
-# 19. Copy /trunk into /tags/{version}, and SVN commit.
-# 20. Delete temporary local SVN checkout.
+#  1. Check main plugin file exists.
+#  2. Check readme.txt version matches main plugin file version.
+#  3. Ask if input is correct, and give chance to abort.
+#  4. Check if Git tag exists for version number (must match exactly).
+#  5. Checkout SVN repo.
+#  6. Set to SVN ignore some GitHub-related files.
+#  7. Export HEAD of master from git to the trunk of SVN.
+#  8. Initialise and update and git submodules.
+#  9. Move /trunk/assets up to /assets.
+# 10. Move into /trunk, and SVN commit.
+# 11. Move into /assets, and SVN commit.
+# 12. Copy /trunk into /tags/{version}, and SVN commit.
+# 13. Delete temporary local SVN checkout.
 
 echo
 echo "WordPress Plugin SVN Deploy v3.0.0"
@@ -31,12 +24,6 @@ echo "Let's collect some information first. There are six questions."
 echo
 echo "Default values are in brackets - just hit enter to accept them."
 echo
-
-# Get some user input
-# Can't use the -i flag for read, since that doesn't work for bash 3
-#printf "Q1. WordPress Repo Plugin Slug e.g. my-awesome-plugin: "
-#read -e PLUGINSLUG
-#echo
 
 # Set up some default values. Feel free to change these in your own script
 CURRENTDIR=$(pwd)
@@ -48,23 +35,11 @@ PLUGINDIRNAME="plugin-files"
 PLUGINDIR="$CURRENTDIR/$PLUGINDIRNAME"
 MAINFILE="$PLUGINSLUG.php"
 
-#echo "Q2. Your local plugin root directory (the Git repo)."
-#printf "($default_plugindir): "
-#read -e  input
-#input="${input%/}" # Strip trailing slash
-#PLUGINDIR="${input:-$default_plugindir}" # Populate with default if empty
-#echo
-
 # Check directory exists.
 if [ ! -d "$PLUGINDIR" ]; then
   echo "Directory $PLUGINDIR not found. Aborting."
   exit 1;
 fi
-
-#printf "Q3. Name of the main plugin file ($default_mainfile): "
-#read -e input
-#MAINFILE="${input:-$default_mainfile}" # Populate with default if empty
-#echo
 
 # Check main plugin file exists.
 if [ ! -f "$PLUGINDIR/$MAINFILE" ]; then
@@ -92,27 +67,6 @@ fi
 
 echo
 
-#echo "Q4. Path to a local directory where a temporary SVN checkout can be made."
-#printf "Don't add trunk ($default_svnpath): "
-#read -e input
-#input="${input%/}" # Strip trailing slash
-#SVNPATH="${input:-$default_svnpath}" # Populate with default if empty
-#echo
-
-#echo "Q5. Remote SVN repo on WordPress.org."
-#printf "($default_svnurl): "
-#read -e input
-#input="${input%/}" # Strip trailing slash
-#SVNURL="${input:-$default_svnurl}" # Populate with default if empty
-#echo
-
-#printf "Q6. Your WordPress repo SVN username ($default_svnuser): "
-#read -e input
-#SVNUSER="${input:-$default_svnuser}" # Populate with default if empty
-#echo
-
-#echo "That's all of the data collected."
-#echo
 echo "Slug: external-login"
 echo "Plugin directory: $PLUGINDIR"
 echo "Main file: $MAINFILE"
@@ -169,7 +123,7 @@ Thumbs.db
 echo "Exporting the HEAD of master from git to the trunk of SVN"
 find $PLUGINDIRNAME -print0 | git checkout-index --prefix=$SVNPATH/trunk/ -f -z --stdin
 mv $SVNPATH/trunk/plugin-files/* $SVNPATH/trunk/
-rmdir $SVNPATH/trunk/plugin-files/
+rm -rf $SVNPATH/trunk/plugin-files/
 
 # If submodule exist, recursively check out their indexes
 if [ -f ".gitmodules" ]
