4e55935025d8e4fec61a7c5fff3f0d85ccac0419
Tom Benyon <Tom.Benyon@creative-assembly.com>
2019-12-30 08:51:48 +0000

    Add acceptance test for local login

---
+++ tests/acceptance/LocalLoginCest.php
@@ -0,0 +1,29 @@
+<?php
+
+class LocalLoginCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+        $I->amOnPage('/wp-login.php?action=logout');
+    }
+
+    public function loginWithCorrectCredentials(AcceptanceTester $I)
+    {
+        $I->amOnPage('/wp-login.php');
+        $I->see('Log In');
+        $I->fillField('#user_login', 'admin');
+        $I->fillField('#user_pass', 'admin');
+        $I->click('#wp-submit');
+        $I->seeInCurrentUrl('wp-admin');
+    }
+
+    public function loginWithIncorrectCredentials(AcceptanceTester $I)
+    {
+        $I->amOnPage('/wp-login.php');
+        $I->see('Log In');
+        $I->fillField('#user_login', 'admin');
+        $I->fillField('#user_pass', 'wrongPassword');
+        $I->click('#wp-submit');
+        $I->seeInCurrentUrl('wp-login');
+    }
+}
