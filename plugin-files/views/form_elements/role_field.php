79229a87371a4186b88a991f375bf06e09e38917
Tom Benyon <tom@benyon.io>
2020-12-05 18:14:26 +0000

    Add safeguards and error logs for Rene test

--- plugin-files/views/form_elements/role_field.php
+++ plugin-files/views/form_elements/role_field.php
@@ -8,10 +8,42 @@
         >
 
         <select class="wordpress_role" name="<?php echo $exlog_external_role[$EXLOG_JSON_KEY_WORDPRESS_NAME]; ?>">
+            <?php
+              error_log('-------EXLOG LOGS FOR RENE role_field ---------');
+
+              error_log('Roles from exlog_get_wp_role_types():');
+              error_log(var_export(exlog_get_wp_role_types(), true));
+
+              error_log('Roles from $EXLOG_WORDPRESS_AVAILABLE_ROLES:');
+              error_log(var_export($EXLOG_WORDPRESS_AVAILABLE_ROLES, true));
+
+              error_log('Is external role set?');
+              error_log(var_export(isset($exlog_external_role), true));
+//              error_log('Re-populating $EXLOG_WORDPRESS_AVAILABLE_ROLES incase this is the issue:');
+//              $EXLOG_WORDPRESS_AVAILABLE_ROLES = exlog_get_wp_role_types();
+//
+//            error_log('Roles from $EXLOG_WORDPRESS_AVAILABLE_ROLES re populated:');
+//            error_log(var_export($EXLOG_WORDPRESS_AVAILABLE_ROLES, true));
+
+
+            ?>
             <?php $EXLOG_WORDPRESS_AVAILABLE_ROLES[EXLOG_ROLE_BLOCK_VALUE] = "-- BLOCK --"; //Add ability to block based on role ?>
+
+
+            <?php
+              error_log('Roles from $EXLOG_WORDPRESS_AVAILABLE_ROLES after adding blocked option:');
+              error_log(var_export($EXLOG_WORDPRESS_AVAILABLE_ROLES, true));
+            ?>
+
+
             <?php foreach ($EXLOG_WORDPRESS_AVAILABLE_ROLES as $key => $value) : ?>
+                <?php
+                  error_log('In loop:');
+                  error_log(var_export($key, true));
+                  error_log(var_export($value, true));
+                ?>
                 <option
-                    <?php if ($exlog_external_role[$EXLOG_JSON_KEY_WORDPRESS_VALUE] == $key) :?>
+                    <?php if (isset($exlog_external_role) && $exlog_external_role[$EXLOG_JSON_KEY_WORDPRESS_VALUE] == $key) :?>
                         selected="selected"
                     <?php endif; ?>
                     value="<?php echo $key; ?>"
@@ -21,6 +53,9 @@
             <?php endforeach; ?>
         </select>
 
+        <?php
+          error_log('-------EXLOG LOGS FOR RENE END ---------');
+        ?>
         <input
             class="remove_role_pairing button button-primary"
             value="Delete"
