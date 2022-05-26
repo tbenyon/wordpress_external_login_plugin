016b605de6f9e3bdb6a8e43c3402a2dd74a6f2f1
Tom Benyon <tom@benyon.io>
2022-05-26 22:42:43 +0100

    Missing keys?

--- build-scripts/ciSetup.sh
+++ build-scripts/ciSetup.sh
@@ -10,7 +10,7 @@ apt-get install curl --assume-yes
 echo -e "\nInstalling PHP"
 apt-get install --assume-yes software-properties-common
 LC_ALL=C.UTF-8 add-apt-repository --yes --update ppa:ondrej/php
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 68980A0EA10B4DE8
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install php7.4 php7.4-curl php7.4-xml php7.4-mbstring php7.4-mysql
 
